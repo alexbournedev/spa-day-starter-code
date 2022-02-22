a4de8d0d9d9d14c90129d59c7b621b4ffc9e57ce
Alex Bourne <afb989@outlook.com>
2022-02-21 21:17:07 -0500

    completed studio + bonus

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,45 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Please Enter a Username.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    private void checkPassword(String password, String verifyPassword){
+        if (!(password.equals(verifyPassword))){
+            if (password != null && verifyPassword != null){
+                verifyPassword = null;
+            }
+
+        }
     }
 
     public String getUsername() {
@@ -38,5 +64,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword(password, getVerifyPassword());
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword(getPassword(), verifyPassword);
     }
 }
